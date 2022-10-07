f9e2936bcffd4826f178c485b1639d86ff8ecac8
Eric K <54124297+Nyveon@users.noreply.github.com>
2022-10-06 23:58:12 -0300

    feat: adds boilerplate for homepage

---
+++ data/departments.js
@@ -0,0 +1,20 @@
+/*jshint esversion: 6 */
+
+const departments = [
+    {
+        code: "DCC",
+        name: "Ingeniería Civil en Computación",
+        image: "./images/placeholder.jpg",
+        logo: "./images/placeholder.jpg",
+    },
+    {
+        code: "Astronomía",
+        name: "Astronomía",
+        image: "./images/placeholder.jpg",
+    },
+    {
+        code: "Física",
+        name: "Física",
+        image: "./images/placeholder.jpg",
+    },
+];
