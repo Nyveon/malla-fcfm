1cb9dacb059c11b8181758f5336daad427b24164
Eric K <54124297+Nyveon@users.noreply.github.com>
2022-10-19 17:56:57 -0300

    chore: fixes filenames and disables js

--- js/malla.js
+++ js/malla.js
@@ -1,12 +1,30 @@
 "use strict";
 // Site by Eric K.
 /* jshint globalstrict: true */
-/* jshint esversion: 6 */
+/* jshint esversion: 8 */
 /* jshint browser: true */
 /* globals navigation */
 
-// Intercept navigation (SPA-ification)
-navigation.addEventListener('navigate', navigateEvent => {
-    navigateEvent.intercept();
-});
 
+// Intercept navigation (SPA-ification)
+/*
+navigation.addEventListener('navigate', navigateEvent => {
+
+    const url = new URL(navigateEvent.destination.url);
+
+    if (url.pathname === '/') {
+        navigateEvent.intercept({
+            async handler() {
+                state.page = "home";
+            }
+        });
+    } else {
+        navigateEvent.intercept({
+            async handler() {
+                state.page = url.pathname.slice(1);
+                console.log(state)
+            }
+        });
+    }
+});
+*/
