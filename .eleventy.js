3a000ba6a5679352b5241834082a7422351cd201
Eric K <54124297+Nyveon@users.noreply.github.com>
2023-06-09 19:55:19 -0400

    feat: malla static generation

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,10 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("./src/css");
 	eleventyConfig.addPassthroughCopy("./src/js");
 
+    eleventyConfig.addFilter('getGlobalData', (data) => {
+        return require(`./src/data/${data}.json`);
+    });
+
 	return {
 		dir: {
 			input: "src",
