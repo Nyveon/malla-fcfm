3de82cf8a3f8f8a76b3729851392363158493c64
Eric K <54124297+Nyveon@users.noreply.github.com>
2023-06-09 19:20:01 -0400

    feat: index static generation

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,14 @@
 module.exports = function (eleventyConfig) {
-    return {
-        dir: {
-            input: "src",
-            data: "data",
-            includes: "includes",
-            output: "docs"
-        }
-    }
-} 
+	eleventyConfig.addPassthroughCopy("./src/images");
+	eleventyConfig.addPassthroughCopy("./src/css");
+	eleventyConfig.addPassthroughCopy("./src/js");
+
+	return {
+		dir: {
+			input: "src",
+			data: "data",
+			includes: "includes",
+			output: "docs",
+		},
+	};
+};
